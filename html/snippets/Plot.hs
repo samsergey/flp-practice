b0282755053c0eec25ac2cc58ac490b37ac2c357
Sergey Nix <samsergey@yandex.ru>
2017-02-07 10:17:15 +1200

    Proceeded with lab5

--- html/snippets/Plot.hs
+++ html/snippets/Plot.hs
@@ -16,13 +16,16 @@ tabulate f (x1,x2) n = tab f <$> [x1,x1+h..x2]
 scale :: Range -> Range -> Double -> Double
 scale (a,b) (c,d) x = (d-c)/(b-a)*(x-a)+c
 
-polyline :: Table -> String
-polyline pts = concat [ "<polyline points='"
-                      , points
-                      , "' fill='none' stroke='blue'/>" ]
+polyline :: Table -> (Range, Range) -> String
+polyline pts _ = concat [ "<polyline points='"
+                        , points
+                        , "' fill='none' stroke='blue'/>" ]
    where
      points = unwords $ showPoint <$> pts
      showPoint (x,y) = show x ++ "," ++ show y
 
-plot :: NumFunc -> (Range, Range) -> Int -> String
-plot f (xRange, yRange) n = undefined
+plot :: NumFunc -> Int -> (Range, Range) -> String
+plot f n (xRange, yRange) = undefined
+
+listPlot :: Table -> (Range, Range) -> String
+listPlot tbl (xRange, yRange) = undefined
