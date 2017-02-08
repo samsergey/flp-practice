2f241f7c784acb23d3b3bff0d06fa0a08d13b362
Sergey B. Samoylenko <samsergey@yandex.ru>
2017-02-08 12:15:26 +1200

    prepared monoidal fractal tree example

--- html/snippets/wc.hs
+++ html/snippets/wc.hs
@@ -1,5 +1,6 @@
 {-# Language Strict #-}
 import Data.Monoid
+import Data.List
 import Control.Arrow
 
 wc :: String -> (Int, Int, Int)
@@ -28,3 +29,35 @@ main = do
   -- txt <- readFile "../emacs-ref.html"
   txt <- readFile "txt"
   print $ wc txt
+
+
+power :: (Monoid a, Integral i) => a -> i -> a
+power _ 0 = mempty
+power a n
+  | r == 0    = b
+  | otherwise = a `mappend` b
+  where
+    (q,r) = n `divMod` 2
+    b = (a `mappend` a) `power` q
+
+
+data M a = M [[a]] | Unit deriving Show
+
+instance Num a => Monoid (M a) where
+  mempty = Unit
+  Unit `mappend` x = x
+  x `mappend` Unit = x
+  M m1 `mappend` M m2 = M [ [ r <.> c |  c <- transpose m2] |r <- m1 ]
+    where a <.> b = sum $ zipWith (*) a b
+
+
+rotate a = M [[cos a, - sin a, 0],[sin a, cos a, 0], [0,0,1]]
+scale s = M [[s, 0, 0],[0, s, 0], [0,0,1]]
+shift x y = M [[1, 0, x],[0, 1, y], [0,0,1]]
+
+transform m v = (<> v) <$> m
+
+model :: [M Double] -> [M Double]
+model = foldMap $ transform [ scale 0.5 <> rotate (-pi/2) <> shift 0 1
+                            , scale 0.5 <> shift 0 1
+                            , scale 0.5 <> rotate (pi/2) <> shift 0 1]
