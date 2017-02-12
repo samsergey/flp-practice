b0825ebfb1da8d9292e37bc8632d8cc90d571eba
Sergey Nix <samsergey@yandex.ru>
2017-02-12 23:14:26 +1200

    Struggled with Actions

--- html/snippets/Picture2.hs
+++ html/snippets/Picture2.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 import Data.Monoid
 import Data.Semigroup hiding ((<>))
 import Data.List.Split (splitOn)
@@ -7,16 +10,15 @@ type Pt = (Float, Float)
   
 data Primitive = Point Pt
                | Line [Pt]
-               | Group String [Primitive]
                deriving Show
 
-data Picture = Picture Box [Primitive] deriving Show
+data Picture = Picture (Box, [Primitive])
+             deriving Show
 
-box (Picture ((Min x1, Min y1), (Max x2, Max y2)) _) = ((x1,y1),(x2,y2))
+box (Picture (((Min x1, Min y1), (Max x2, Max y2)), _)) = ((x1,y1),(x2,y2))
 width p = x2 - x1 where ((x1,_),(x2,_)) = box p
 height p = y2 - y1 where ((_,y1),(_,y2)) = box p
-contents (Picture _ p) = p
-
+contents (Picture (_, p)) = p
 
 type Box = ((Min Float, Min Float), (Max Float, Max Float))
 
@@ -25,10 +27,10 @@ instance Bounded Float where
   maxBound = 1000
 
 instance Monoid Picture where
-  mempty = Picture mempty mempty
-  Picture b1 p1 `mappend` Picture b2 p2 = Picture (b1 <> b2) (p1 ++ p2)
+  mempty = Picture mempty 
+  Picture p1 `mappend` Picture p2 = Picture (p1 <> p2)
   
-makePicture p = Picture (findBox p) [p]
+makePicture p = Picture (findBox p, [p])
 
 findBox p = case p of
   Line pts -> foldMap box pts
@@ -84,35 +86,35 @@ instance Num a => Monoid (M a) where
 
 --------------------------------------------------------------------------------
 
-data Trans = Id
-           | Trans :+: Trans
-           | Affine (M Float)
-           | Attrib (String) deriving Show
+data Tr = Id | Affine (M Float) deriving Show
 
-instance Monoid Trans where
+instance Monoid Tr where
   mempty = Id
-  Id `mappend` t = t
-  t `mappend` Id = t
-  Affine m1 `mappend` Affine m2 = Affine (m1 <> m2)
-  Affine m1 `mappend` (Affine m2 :+: t) = Affine (m1 <> m2) :+: t
-  Attrib s1 `mappend` Attrib s2 = Attrib (s1 <> s2)
-  Attrib s1 `mappend` (Attrib s2 :+: t) = Attrib (s1 <> s2) :+: t
-  t1 :+: t2 `mappend` ts = t1 :+: (t2 :+: ts)
-  t `mappend` ts = t :+: ts
+  Id `mappend` x = x
+  x `mappend` Id = x
+  Affine m1 `mappend` Affine m2 = Affine $ m1 <> m2
 
-(%) :: Trans -> Picture -> Picture
-Id % p = p
-(t1 :+: ts) % p = t1 % (ts % p)
-Attrib s % (Picture b [Group a ps]) = Picture b [Group (s <> a) ps]
-Attrib s % (Picture b p) = Picture b [Group s p]
-Affine m % (Picture b p) = Picture (foldMap findBox p') p' 
-  where
-    p' = transform <$> p
-    transform p = case p of
-      Point pt   -> Point $ trans pt
-      Line pts   -> Line $ trans <$> pts
-      Group a ps -> Group a $ transform <$> ps
-    trans (x,y) = let M [[x'],[y'],_] = m <> M [[x],[y],[1]] in (x',y')
+Id .$ p = p
+Affine m .$ p = affine m p
+
+class Affine a where
+  affine :: (M Float) -> a -> a
+
+instance Affine (Float,Float) where
+  affine m (x,y) = let M [[x'],[y'],_] = m <> M [[x],[y],[1]]
+                      in (x',y')
+
+instance Affine Primitive where
+  affine m p = case p of
+    Point pt   -> Point $ affine m pt
+    Line pts   -> Line $ affine m <$> pts
+
+instance Affine Picture where
+  affine m p = Picture (b, ps')
+    where
+      b = foldMap findBox ps'
+      ps' = affine m <$> contents p
+
 
 shift x y = Affine $ M [[1,0,x],[0,1,y],[0,0,1]]
 rotate a = Affine $ M [[c,-s,0],[s,c,0],[0,0,1]]
@@ -120,6 +122,4 @@ rotate a = Affine $ M [[c,-s,0],[s,c,0],[0,0,1]]
         s = sin phi
         phi = 180*a/pi
         
-color c = Attrib $ format " stroke='_'" [c]
-
 main = pure ()
