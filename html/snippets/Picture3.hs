0a0e263d4b47e45f7810959ef9ff603cfdb4a474
Sergey B. Samoylenko <samsergey@yandex.ru>
2017-02-14 16:49:22 +1200

    proceed lab 6

--- html/snippets/Picture3.hs
+++ html/snippets/Picture3.hs
@@ -18,14 +18,24 @@ data Primitive = Point Pt
                | Line [Pt]
                deriving Show
 
-data Picture = Picture (Convex, [Primitive])
-             | Picture :+: Picture
-             | Transform :*: Picture
+type Box = ((Min Float, Min Float), (Max Float, Max Float))
+
+data Picture = Picture (Box, [Primitive])
+             | Transform :$: Picture
              deriving Show
 
-box (Picture (Convex c,_)) = ((x1,y1),(x2,y2))
-  where box (x,y)= ((Min x, Min y), (Max x, Max y))
-        ((Min x1, Min y1), (Max x2, Max y2)) = foldMap box c
+transform p = case p of
+  Picture p -> Picture p
+  Affine m :$: Picture p -> affine m (Picture p)
+  t1 :$: (t2 :$: p) -> transform $ (t1 <> t2) :$: p
+
+instance Monoid Picture where
+  mempty = Picture mempty 
+  Picture p1 `mappend` Picture p2 = Picture (p1 <> p2)
+  p1 `mappend` p2 = transform p1 <> transform p2
+ 
+box p = let Picture (((Min x1, Min y1), (Max x2, Max y2)),_) = transform p
+        in ((x1,y1),(x2,y2))
         
 width p = x2 - x1 where ((x1,_),(x2,_)) = box p
 height p = y2 - y1 where ((_,y1),(_,y2)) = box p
@@ -36,24 +46,21 @@ upper = fst
 right = snd
 left  = fst
 contents (Picture (_, p)) = p
-
-type Box = ((Min Float, Min Float), (Max Float, Max Float))
+contents p = contents (transform p)
 
 instance Bounded Float where
   minBound = -1000
   maxBound = 1000
-
-instance Monoid Picture where
-  mempty = Picture mempty 
-  p1 `mappend` p2 = p1 :+: p2
   
-picture p = Picture (findConvex p, [p])
-  where
-    findConvex p = case p of
-      Line pts -> convex pts
-      Point pt -> convex [pt]
+picture p = Picture (findBox p, [p])
 
+findBox p = case p of
+  Line pts -> foldMap box pts
+  Point pt -> box pt
+  where box (x,y)= ((Min x, Min y), (Max x, Max y))
+  
 point pt = picture $ Point pt
+
 line pts = picture $ Line pts
 
 square a = picture $ Line [(0,0), (a,0), (a,a), (0,a), (0,0)]
@@ -82,7 +89,7 @@ instance SVG Primitive where
 instance SVG Picture where
   toSVG p =
     format "<svg width='_' height='_' fill='none' stroke='blue'>_</svg>"
-    [show (width p), show (height p), foldMap toSVG (contents (adjust p))]
+    [show (width p), show (height p), foldMap toSVG (contents (transform (adjust p)))]
 
 -- house = walls <> roof
 --   where
@@ -103,39 +110,41 @@ instance Num a => Monoid (M a) where
     where a `dot` b = sum $ zipWith (*) a b
 
 --------------------------------------------------------------------------------
-type Transform = M Float
+data Transform = Affine (M Float) deriving Show
+
+instance Monoid Transform where
+  mempty = Affine mempty
+  Affine m1 `mappend` Affine m2 = Affine (m1 <> m2)
 
 class Affine a where
-  ($.) :: M Float -> a -> a
+  affine :: M Float -> a -> a
 
 instance Affine (Float,Float) where
-  m $. (x,y) = let M [[x'],[y'],_] = m <> M [[x],[y],[1]]
+  affine m (x,y) = let M [[x'],[y'],_] = m <> M [[x],[y],[1]]
                    in (x',y')
 
 instance Affine Primitive where
-  m $. p = case p of
-    Point pt   -> Point $  m $. pt
-    Line pts   -> Line $ (m $.) <$> pts
-
-instance Affine Convex where
-  m $. (Convex c) = Convex $ (m $.) <$> c
+  affine m p = case p of
+    Point pt   -> Point $  affine m pt
+    Line pts   -> Line $ (affine m) <$> pts
 
 instance Affine Picture where
-  m $. (Picture (c,ps)) = Picture (m $.c, (m $.) <$> ps)
+  affine m (Picture (_,ps)) = let p' = affine m <$> ps
+                              in Picture (foldMap findBox p', p')
 
-shift x y = M [[1,0,x],[0,1,y],[0,0,1]] 
-scaleX a = M [[a,0,0],[0,1,0],[0,0,1]]
-scaleY a = M [[1,0,0],[0,a,0],[0,0,1]]
-scale a = M [[a,0,0],[0,a,0],[0,0,1]]
-rotate a = M [[c,-s,0],[s,c,0],[0,0,1]]
+shift x y = (Affine (M [[1,0,x],[0,1,y],[0,0,1]]) :$:)
+scaleX a = (Affine (M [[a,0,0],[0,1,0],[0,0,1]]) :$:)
+scaleY a = (Affine (M [[1,0,0],[0,a,0],[0,0,1]]) :$:)
+scale a = (Affine (M [[a,0,0],[0,a,0],[0,0,1]]) :$:)
+rotate a = (Affine (M [[c,-s,0],[s,c,0],[0,0,1]]) :$:)
   where c = cos phi
         s = sin phi
         phi = pi*a/180
 
-p `at` (x,y) = shift (x-x1) (y-y1) $. p
+p `at` (x,y) = shift (x-x1) (y-y1) p
    where (x1,y1) = (left.lower.corner) p
 
-adjust p = (scaleY (-1) $. p) `at` (0,0)
+adjust p = (scaleY (-1) p) `at` (0,0)
 
 p1 `beside` p2 = p1 <> p2 `at` (right.lower.corner) p1
 p1 `above` p2 = p1 `at` (left.upper.corner) p2 <> p2 
@@ -144,22 +153,3 @@ row ps = foldl beside mempty ps
 col ps = foldr above mempty ps
 
 main = pure ()
-
-data Convex = Convex [Pt] deriving Show
-
-instance Monoid Convex where
-  mempty = Convex []
-  Convex c1 `mappend` Convex c2 = convex (c1 ++ c2)
-
-convex [] = Convex []
-convex [p] = Convex [p]
-convex pts = Convex $ chain [] sorted ++ chain [] (reverse sorted)
-  where
-    sorted = sort pts
-    chain acc [] = tail acc
-    chain acc@(r1:r2:rs) (x:xs) | clockwise r2 r1 x = chain (r2:rs) (x:xs)
-                                | otherwise = chain (x:acc) xs
-    chain acc (x:xs) = chain (x:acc) xs      
-    clockwise o a b = (a `sub` o) `cross` (b `sub` o) <= 0
-    cross (x1, y1) (x2, y2) = x1 * y2 - x2 * y1
-    sub (x1, y1) (x2, y2) = (x1 - x2, y1 - y2)
