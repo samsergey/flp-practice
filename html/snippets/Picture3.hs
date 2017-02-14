85b73b87e325ff4e0899f9b7d36d5bfaad637483
Sergey Nix <samsergey@yandex.ru>
2017-02-14 22:21:27 +1200

    added Boxed class to lab 6 added Styles to Pictures3.hs

--- html/snippets/Picture3.hs
+++ html/snippets/Picture3.hs
@@ -3,6 +3,7 @@ import Data.Monoid
 import Data.Semigroup (Min(..), Max(..))
 import Data.List.Split (splitOn)
 import Data.List (transpose, sort)
+import qualified Data.Map as M
 
 times :: (Monoid a, Integral i) => i -> a -> a
 0 `times` _ = mempty
@@ -16,64 +17,99 @@ type Pt = (Float, Float)
   
 data Primitive = Point Pt
                | Line [Pt]
+               | Circle Pt Float
+               | Group Attributes [Primitive]
                deriving Show
 
+type Attributes  = M.Map String String
+
 type Box = ((Min Float, Min Float), (Max Float, Max Float))
+
+class Boxed a where
+  box :: a -> Box
+
+  width :: a -> Float
+  width p = x2 - x1 where ((Min x1,_),(Max x2,_)) = box p
+
+  height :: a -> Float
+  height p = y2 - y1 where ((_,Min y1),(_,Max y2)) = box p
+
+  corner :: a -> ((Pt,Pt),(Pt,Pt))
+  corner p = (((x1,y2),(x2,y2)),((x1,y1),(x2,y1)))
+    where ((Min x1,Min y1),(Max x2, Max y2)) = box p
+
+lower = snd
+upper = fst
+right = snd
+left  = fst
+
+instance Boxed Primitive where
+  box p = case p of
+    Line pts -> foldMap box pts
+    Point pt -> box pt
+    Circle (x,y) r -> box $ Line [(x-r,y-r),(x+r,y+r)]
+    Group _ g -> foldMap box g
+
+instance Boxed Pt where
+  box (x,y)= ((Min x, Min y), (Max x, Max y))
+  width _ = 0
+  height _ = 0
 
 data Picture = Picture (Box, [Primitive])
              | Transform :$: Picture
              deriving Show
 
-transform p = case p of
+contents :: Picture -> [Primitive]
+contents (Picture (_, p)) = p
+contents p = contents (transform p)
+
+transform :: Picture -> Picture
+transform pic = case pic of
+  (t1 :+: t2) :$: p -> transform $ t1 :$: transform (t2 :$: p)
+  t1 :$: (t2 :$: p) -> transform $ (t1 <> t2) :$: p
   Picture p -> Picture p
   Affine m :$: Picture p -> affine m (Picture p)
-  t1 :$: (t2 :$: p) -> transform $ (t1 <> t2) :$: p
+  Style s :$: Picture (b,p) -> Picture (b, [Group s p])
 
 instance Monoid Picture where
   mempty = Picture mempty 
   Picture p1 `mappend` Picture p2 = Picture (p1 <> p2)
   p1 `mappend` p2 = transform p1 <> transform p2
- 
-box p = let Picture (((Min x1, Min y1), (Max x2, Max y2)),_) = transform p
-        in ((x1,y1),(x2,y2))
-        
-width p = x2 - x1 where ((x1,_),(x2,_)) = box p
-height p = y2 - y1 where ((_,y1),(_,y2)) = box p
-corner p = (((x1,y2),(x2,y2)),((x1,y1),(x2,y1)))
-  where ((x1,y1),(x2,y2)) = box p
-lower = snd
-upper = fst
-right = snd
-left  = fst
-contents (Picture (_, p)) = p
-contents p = contents (transform p)
+
+instance Boxed Picture where
+  box p = b where Picture (b,_) = transform p
 
 instance Bounded Float where
   minBound = -1000
   maxBound = 1000
   
-picture p = Picture (findBox p, [p])
+primitive :: Primitive -> Picture
+primitive p = Picture (box p, [p])
 
-findBox p = case p of
-  Line pts -> foldMap box pts
-  Point pt -> box pt
-  where box (x,y)= ((Min x, Min y), (Max x, Max y))
-  
-point pt = picture $ Point pt
+point :: Pt -> Picture
+point pt = primitive $ Point pt
 
-line pts = picture $ Line pts
+line :: [Pt] -> Picture
+line pts = primitive $ Line pts
 
-square a = picture $ Line [(0,0), (a,0), (a,a), (0,a), (0,0)]
+circle :: Float -> Picture
+circle r = primitive $ Circle (0,0) r
 
-rectangle a b = picture $ Line [(0,0), (a,0), (a,b), (0,b), (0,0)]
+square :: Float -> Picture
+square a = primitive $ Line [(0,0), (a,0), (a,a), (0,a), (0,0)]
 
-triangle a h = picture $ Line [(0,0), (a,0), (a/2,h), (0,0)]
+rectangle :: Float -> Float -> Picture
+rectangle a b = primitive $ Line [(0,0), (a,0), (a,b), (0,b), (0,0)]
+
+triangle :: Float -> Float -> Picture
+triangle a h = primitive $ Line [(0,0), (a,0), (a/2,h), (0,0)]
 
 --------------------------------------------------------------------------------
 
 format :: String -> [String] -> String
 format s args = mconcat $ zipWith (<>) (splitOn "_" s) (args <> [""])
 
+writeSVG :: SVG a => FilePath -> a -> IO ()
 writeSVG f = writeFile f . toSVG
 
 class SVG a where
@@ -83,6 +119,8 @@ instance SVG Primitive where
   toSVG p = case p of
     Point (x,y) -> format "<circle fill='blue' stroke='none' cx='_' cy='_' r='1'/>" [show x, show y]
     Line pts -> format "<polyline points='_'/>" [foldMap showPt pts]
+    Circle (x,y) r -> format "<circle cx='_' cy='_' r='_'/>" [show x, show y, show r]
+    Group s g -> format "<g _>_</g>" [toSVG s, foldMap toSVG g]
     where 
       showPt (x,y) = format " _,_" [show x, show y]
 
@@ -90,6 +128,10 @@ instance SVG Picture where
   toSVG p =
     format "<svg width='_' height='_' fill='none' stroke='blue'>_</svg>"
     [show (width p), show (height p), foldMap toSVG (contents (transform (adjust p)))]
+
+instance SVG Attributes where
+  toSVG attr = foldMap (\(k,v) -> format " _='_'" [k, v]) $ M.toList attr
+
 
 -- house = walls <> roof
 --   where
@@ -110,11 +152,20 @@ instance Num a => Monoid (M a) where
     where a `dot` b = sum $ zipWith (*) a b
 
 --------------------------------------------------------------------------------
-data Transform = Affine (M Float) deriving Show
+data Transform = Affine (M Float)
+               | Style Attributes
+               | Transform :+: Transform
+               deriving Show
 
+infixr 4 :+:
+infixr 4 :$:
+  
 instance Monoid Transform where
   mempty = Affine mempty
   Affine m1 `mappend` Affine m2 = Affine (m1 <> m2)
+  Style s1 `mappend` Style s2 = Style (s1 <> s2)
+  t `mappend` Style s = Style s :+: t
+  t1 `mappend` t2 = t1 :+: t2
 
 class Affine a where
   affine :: M Float -> a -> a
@@ -125,31 +176,69 @@ instance Affine (Float,Float) where
 
 instance Affine Primitive where
   affine m p = case p of
-    Point pt   -> Point $  affine m pt
-    Line pts   -> Line $ (affine m) <$> pts
+    Point pt    -> Point $ affine m pt
+    Line pts    -> Line $ (affine m) <$> pts
+    Circle pt r -> Circle (affine m pt) r
+    Group s g   -> Group s (affine m <$> g)
 
 instance Affine Picture where
   affine m (Picture (_,ps)) = let p' = affine m <$> ps
-                              in Picture (foldMap findBox p', p')
+                              in Picture (foldMap box p', p')
+  affine m p = affine m (transform p)
 
+
+shift :: Float -> Float -> Picture -> Picture
 shift x y = (Affine (M [[1,0,x],[0,1,y],[0,0,1]]) :$:)
+
+scaleX :: Float -> Picture -> Picture
 scaleX a = (Affine (M [[a,0,0],[0,1,0],[0,0,1]]) :$:)
+
+scaleY :: Float -> Picture -> Picture
 scaleY a = (Affine (M [[1,0,0],[0,a,0],[0,0,1]]) :$:)
+
+scale :: Float -> Picture -> Picture
 scale a = (Affine (M [[a,0,0],[0,a,0],[0,0,1]]) :$:)
+
+rotate :: Float -> Picture -> Picture
 rotate a = (Affine (M [[c,-s,0],[s,c,0],[0,0,1]]) :$:)
   where c = cos phi
         s = sin phi
         phi = pi*a/180
 
+at :: Picture -> (Float, Float) -> Picture
 p `at` (x,y) = shift (x-x1) (y-y1) p
    where (x1,y1) = (left.lower.corner) p
 
+adjust :: Picture -> Picture
 adjust p = (scaleY (-1) p) `at` (0,0)
 
-p1 `beside` p2 = p1 <> p2 `at` (right.lower.corner) p1
-p1 `above` p2 = p1 `at` (left.upper.corner) p2 <> p2 
+beside :: Picture -> Picture -> Picture
+beside p1 p2 = p1 <> p2 `at` (right.lower.corner) p1
 
+above :: Picture -> Picture -> Picture
+above p1 p2 = p1 `at` (left.upper.corner) p2 <> p2 
+
+row :: Foldable t => t Picture -> Picture
 row ps = foldl beside mempty ps
+
+col :: Foldable t => t Picture -> Picture
 col ps = foldr above mempty ps
 
+
+makeAttr :: String -> String -> Picture -> Picture
+makeAttr k v = (Style (M.singleton k v) :$:)
+
+color :: String -> Picture -> Picture
+color = makeAttr "stroke"
+
+fill :: String -> Picture -> Picture
+fill = makeAttr "fill"
+
+lineWidth :: Show a => a -> Picture -> Picture
+lineWidth w = makeAttr "lineWidth" $ show w
+
+opacity :: Show a => a -> Picture -> Picture
+opacity a = (makeAttr "stroke-opacity" <> makeAttr "fill-opacity") (show a) 
+
+main :: IO ()
 main = pure ()
