23d5ff26ee4f86686bd2b51f6a407092959ba4e6
Sergey Nix <samsergey@yandex.ru>
2017-03-04 11:42:08 +1200

    Completed DeMorgan example

--- html/snippets/DeMorgan.hs
+++ html/snippets/DeMorgan.hs
@@ -88,11 +88,22 @@ data Element = R Double
              | L Double
              deriving Show
 
-data Semiring a = Zero | One | Elem a
-  | Plus (Semiring a) (Semiring a)
-  | Times (Semiring a) (Semiring a)
-  | Inv (Semiring a)
+
+data Semiring a = Zero
+                | One
+                | Elem a
+                | Plus (Semiring a) (Semiring a)
+                | Times (Semiring a) (Semiring a)
+                | Inv (Semiring a)
   deriving (Show, Functor)
+
+instance Num a => Num (Semiring a) where
+  fromInteger n = Elem (fromInteger n)
+  (+) = (<|>)
+  (*) = (<->)
+  signum = fmap signum
+  abs = fmap abs
+  negate = fmap negate
 
 instance DeMorgan (Semiring a) where
   zero = Zero
@@ -101,14 +112,14 @@ instance DeMorgan (Semiring a) where
   (<|>) = Plus
   inv = Inv
 
-alg Zero = zero
-alg One = one
-alg (Elem b) = b
-alg (Plus a b) = alg a <|> alg b
-alg (Times a b) = alg a <-> alg b
-alg (Inv a) = inv (alg a)
+reduceDM Zero = zero
+reduceDM One = one
+reduceDM (Elem b) = b
+reduceDM (Plus a b) = reduceDM a <|> reduceDM b
+reduceDM (Times a b) = reduceDM a <-> reduceDM b
+reduceDM (Inv a) = inv (reduceDM a)
 
-foldWith w f x = alg $ (fmap w . f) <$> x
+reduceDMWith w f x = reduceDM $ (fmap w . f) <$> x
 
 r = Elem . R
 c = Elem . C
@@ -116,23 +127,59 @@ l = Elem . L
 k True = Zero
 k False = One
 
-resistance = fmap getFrac . foldWith Frac f
+resistance = fmap getFrac . reduceDMWith Frac f
   where f (R r) = Value r
         f (C _) = Break
         f (L _) = Short
 
-connected = alg . fmap f
+connected = reduceDM . fmap f
   where f (R _) = True
         f (C _) = False
         f (L _) = True
 
-capacity = fmap getInvFrac . foldWith InvFrac f
+capacity = fmap getInvFrac . reduceDMWith InvFrac f
   where f (R _) = Short
         f (C c) = Value c
         f (L _) = Short
 
-impedance s w = fmap getFrac . foldWith Frac f $ s
+impedance s w = fmap getFrac . reduceDMWith Frac f $ s
   where f (R r) = Value $ 0 :+ r
         f (C c) = Value $ 0 :+ (-1)/(w*c)
         f (L l) = Value $ 0 :+ (w*l)
+
+------------------------------------------------------------
+
+insl h s l = Elem (l/(s*h))
+surf a s = Elem (1/(s*a))
+concrete = insl 0.5
+glass = insl 1.05
+air = insl 0.024
+pp = insl 0.07
+edge = surf 5
+
+flux s dT = 40 / (getInvFrac . reduceDM $ InvFrac <$> s)
+
+system = edge 1 <-> (wall <|> window) <-> edge 1
+  where
+    wall = concrete 2 0.5 <-> pp 2 0.1
+    window = glass 1 0.05 <-> air 1 0.1 <-> glass 1 0.05
+
+heater = flux (surf 50 0.25) 60
+
+------------------------------------------------------------
+
+distribute (Times a (Plus b c)) = distribute $ (a <-> b) <|> (a <-> c)
+distribute (Times (Plus a b) c) = distribute $ (a <-> c) <|> (b <-> c)
+distribute (Times a b) = distribute a <-> distribute b
+distribute (Plus a b) = distribute a <|> distribute b
+distribute x = x
+
+
+collect :: Semiring a -> [[a]]
+collect = reduceDM . fmap (\x -> [[x]]) . distribute
+
+links i = nub . concat . filter (i `elem`) . collect
+
+a --> b = a <-> b
+a <+> b = a <|> b
 
