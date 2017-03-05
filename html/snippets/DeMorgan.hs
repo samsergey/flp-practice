9d7cebd4f13e5e47397bfd92eb4d8312dc492f1b
Sergey Nix <samsergey@yandex.ru>
2017-03-05 12:24:45 +1200

    Started de Morgan.html

--- html/snippets/DeMorgan.hs
+++ html/snippets/DeMorgan.hs
@@ -1,5 +1,7 @@
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE DeriveFunctor,GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE
+  FlexibleInstances,
+  DeriveFunctor,
+  GeneralizedNewtypeDeriving #-}
 import Data.Complex
 import Data.List
 import Data.Bool
@@ -10,13 +12,14 @@ class DeMorgan a where
   inv :: a -> a
   (<->) :: a -> a -> a
   (<|>) :: a -> a -> a
-  row :: Foldable t => t a -> a
-  col :: Foldable t => t a -> a
 
   a <-> b = inv (inv a <|> inv b)
   a <|> b = inv (inv a <-> inv b)
   one = inv zero
   zero = inv one
+
+  row :: Foldable t => t a -> a
+  col :: Foldable t => t a -> a
   row = foldl1 (<->)
   col = foldl1 (<|>)
   {-# MINIMAL inv,((<->)|(<|>)),(zero|one) #-}
@@ -26,33 +29,38 @@ instance DeMorgan Bool where
   inv = not
   (<->) = (&&)
 
-newtype Frac a = Frac {getFrac :: a}
-  deriving (Show,Num,Fractional)
-
-instance (Eq a, Fractional a) => DeMorgan (Frac a) where
-  zero = 0
-  inv = (1/)
-  (<->) = (+)
-
-newtype InvFrac a = InvFrac {getInvFrac :: a}
-  deriving (Show,Num,Fractional)
-
-instance (Eq a, Fractional a) => DeMorgan (InvFrac a) where
-  zero = 0
-  inv = (1/)
-  (<|>) = (+)
-
 instance DeMorgan [[a]] where
   zero = [[]]
   (<|>) = (++)
   inv = transpose
 
-table f vals = (a <-> h) <|> (w <-> c)
+table f vals = (a <-> h)
+               <|>
+               (inv h <-> c)
   where
     a = [[" "]]
     h = [show <$> vals]
-    w = inv h
-    c = [ [show $ f x y | x <- vals ] | y <- vals ]
+    c = [show <$> [ f x y | x <- vals] | y <- vals]
+
+showTable tbl = putStrLn . unlines $ unwords <$> tbl
+
+newtype Frac a = Frac {getFrac :: a}
+  deriving (Show, Num, Eq, Ord, Fractional, Functor)
+
+instance Fractional a => DeMorgan (Frac a) where
+  zero = 0
+  inv = (1/)
+  (<->) = (+)
+
+newtype Inv a = Inv {getInv :: a}
+  deriving (Show, Functor)
+
+instance DeMorgan a => DeMorgan (Inv a) where
+  zero = Inv one
+  inv x = inv <$> x
+  Inv a <|> Inv b = Inv $ a <-> b
+  Inv a <-> Inv b = Inv $ a <|> b
+
 
 instance (DeMorgan b) => DeMorgan (a -> b) where
   inv f = inv <$> f
@@ -63,12 +71,12 @@ instance (DeMorgan b) => DeMorgan (a -> b) where
 
 ------------------------------------------------------------
 
-data Range a = Short
-             | Value a
-             | Break
+data Resistance a = Short
+                  | Value a
+                  | Break
   deriving (Show,Eq,Functor)
 
-instance DeMorgan s => DeMorgan (Range s) where
+instance DeMorgan s => DeMorgan (Resistance s) where
   zero = Short
 
   Value r1 <-> Value r2 = Value $ r1 <-> r2
@@ -88,14 +96,12 @@ data Element = R Double
              | L Double
              deriving Show
 
-
 data Semiring a = Zero
                 | One
                 | Elem a
                 | Plus (Semiring a) (Semiring a)
                 | Times (Semiring a) (Semiring a)
-                | Inv (Semiring a)
-  deriving (Show, Functor)
+  deriving (Show, Eq, Functor)
 
 instance Num a => Num (Semiring a) where
   fromInteger n = Elem (fromInteger n)
@@ -110,14 +116,13 @@ instance DeMorgan (Semiring a) where
   one = One
   (<->) = Times
   (<|>) = Plus
-  inv = Inv
+  inv = id
 
 reduceDM Zero = zero
 reduceDM One = one
 reduceDM (Elem b) = b
 reduceDM (Plus a b) = reduceDM a <|> reduceDM b
 reduceDM (Times a b) = reduceDM a <-> reduceDM b
-reduceDM (Inv a) = inv (reduceDM a)
 
 reduceDMWith w f x = reduceDM $ (fmap w . f) <$> x
 
@@ -137,49 +142,53 @@ connected = reduceDM . fmap f
         f (C _) = False
         f (L _) = True
 
-capacity = fmap getInvFrac . reduceDMWith InvFrac f
+capacity = fmap (getFrac . getInv) . reduceDMWith (Inv . Frac) f
   where f (R _) = Short
         f (C c) = Value c
         f (L _) = Short
 
 impedance s w = fmap getFrac . reduceDMWith Frac f $ s
-  where f (R r) = Value $ 0 :+ r
+  where f (R r) = Value $ r :+ 0
         f (C c) = Value $ 0 :+ (-1)/(w*c)
         f (L l) = Value $ 0 :+ (w*l)
 
+s = r 10 <-> ((r 2 <-> l 5e-13) <|> c 10e-9)
+
 ------------------------------------------------------------
 
-insl h s l = Elem (l/(s*h))
-surf a s = Elem (1/(s*a))
-concrete = insl 0.5
-glass = insl 1.05
-air = insl 0.024
-pp = insl 0.07
-edge = surf 5
+insulation h s l = Inv . Frac $ l/(s*h)
+surface a s = Inv . Frac $ 1/(s*a)
 
-flux s dT = 40 / (getInvFrac . reduceDM $ InvFrac <$> s)
+concrete = insulation 0.5
+glass = insulation 1.05
+air = insulation 0.024
+pp = insulation 0.07
+edge = surface 5
+
+flux r dT = dT / getFrac (getInv r)
 
 system = edge 1 <-> (wall <|> window) <-> edge 1
   where
     wall = concrete 2 0.5 <-> pp 2 0.1
     window = glass 1 0.05 <-> air 1 0.1 <-> glass 1 0.05
 
-heater = flux (surf 50 0.25) 60
+--heater = flux (surface 50 0.25) 60
 
 ------------------------------------------------------------
 
 distribute (Times a (Plus b c)) = distribute $ (a <-> b) <|> (a <-> c)
 distribute (Times (Plus a b) c) = distribute $ (a <-> c) <|> (b <-> c)
+distribute (Times (Times a b) c) = distribute $ a <-> (c <-> b)
 distribute (Times a b) = distribute a <-> distribute b
 distribute (Plus a b) = distribute a <|> distribute b
 distribute x = x
 
-
-collect :: Semiring a -> [[a]]
-collect = reduceDM . fmap (\x -> [[x]]) . distribute
-
-links i = nub . concat . filter (i `elem`) . collect
+edges :: Semiring a -> [(a,a)]
+edges = foldMap links . reduceDM . fmap (\x -> [[x]]) . distribute
+  where links lst = zip lst (tail lst)
 
 a --> b = a <-> b
 a <+> b = a <|> b
 
+g :: Semiring Int
+g = (1 --> (3 --> 4)) <+> (2 --> (1 <+> 3)) 
