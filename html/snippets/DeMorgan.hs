1108e8e0cbfaabb9c2a6c0c1678a9bf7cdf5c098
Sergey Nix <samsergey@yandex.ru>
2017-03-10 00:07:29 +1200

    Proceed with de Morgan

--- html/snippets/DeMorgan.hs
+++ html/snippets/DeMorgan.hs
@@ -1,32 +1,27 @@
 {-# LANGUAGE
-  FlexibleInstances,
-  FlexibleContexts,
   DeriveFunctor,
+  FlexibleInstances,
   GeneralizedNewtypeDeriving #-}
 import Data.Complex
-import Data.List
-import Data.Bool
+import Data.List (transpose)
 import Control.Applicative
-import Control.Monad
-
+import Data.Foldable
 
 class DeMorgan a where
-  zero :: a
-  one :: a
+  {-# MINIMAL inv,((<-->)|(<||>)),(zero|one) #-}
   inv :: a -> a
-  (<-->) :: a -> a -> a
-  (<||>) :: a -> a -> a
 
-  a <--> b = inv (inv a <||> inv b)
-  a <||> b = inv (inv a <--> inv b)
-  one = inv zero
+  zero :: a
   zero = inv one
 
-  row :: Foldable t => t a -> a
-  col :: Foldable t => t a -> a
-  row = foldl1 (<-->)
-  col = foldl1 (<||>)
-  {-# MINIMAL inv,((<-->)|(<||>)),(zero|one) #-}
+  one :: a
+  one = inv zero
+
+  (<-->) :: a -> a -> a
+  a <--> b = inv (inv a <||> inv b)
+
+  (<||>) :: a -> a -> a
+  a <||> b = inv (inv a <--> inv b)
 
 instance DeMorgan Bool where
   zero = False
@@ -36,8 +31,10 @@ instance DeMorgan Bool where
 instance DeMorgan [[a]] where
   zero = [[]]
   (<||>) = (++)
+  (<-->) = zipWith (++)
   inv = transpose
 
+table :: (Show a1, Show a) => (a1 -> a1 -> a) -> [a1] -> [[String]]
 table f vals = (a <--> h)
                <||>
                (inv h <--> c)
@@ -46,9 +43,10 @@ table f vals = (a <--> h)
     h = [show <$> vals]
     c = [show <$> [ f x y | x <- vals] | y <- vals]
 
+showTable :: [[String]] -> IO ()
 showTable tbl = putStrLn . unlines $ unwords <$> tbl
 
-newtype Frac a = Frac {getFrac :: a}
+newtype Frac a = Frac { getFrac :: a}
   deriving (Show, Num, Eq, Ord, Fractional, Functor)
 
 instance Fractional a => DeMorgan (Frac a) where
@@ -56,8 +54,8 @@ instance Fractional a => DeMorgan (Frac a) where
   inv = (1/)
   (<-->) = (+)
 
-newtype Dual a = Dual {getDual :: a}
-  deriving (Show, Functor,Eq,Ord)
+newtype Dual a = Dual { getDual :: a}
+  deriving (Show, Functor, Eq, Ord)
 
 instance DeMorgan a => DeMorgan (Dual a) where
   zero = Dual one
@@ -73,135 +71,155 @@ instance DeMorgan b => DeMorgan (a -> b) where
   (<||>) = liftA2 (<||>)
   (<-->) = liftA2 (<-->)
 
-
 ------------------------------------------------------------
 
 data Lumped a = Short
               | Value a
               | Break
-  deriving (Show,Eq,Functor)
+  deriving (Show, Functor)
+
 
 instance DeMorgan s => DeMorgan (Lumped s) where
-  zero = Short
+  zero = Break
 
-  Value r1 <--> Value r2 = Value $ r1 <--> r2
-  Short <--> r = r
-  r <--> Short = r
+  Value x1 <--> Value x2 = Value $ x1 <--> x2
+  Short <--> x = x
+  x <--> Short = x
   _ <--> Break = Break
   Break <--> _ = Break
 
   inv Short = Break
   inv Break = Short
-  inv (Value r) = Value (inv r)
+  inv (Value x) = Value (inv x)
 
 ------------------------------------------------------------
 
 data Circuit a = Zero
                | One
                | Elem a
+               | Inv (Circuit a)
                | Par (Circuit a) (Circuit a)
                | Seq (Circuit a) (Circuit a)
-  deriving (Show, Eq, Functor)
+  deriving (Show, Functor)
 
 instance DeMorgan (Circuit a) where
   zero = Zero
   one = One
   (<-->) = Seq
   (<||>) = Par
-  inv = id
+  inv = Inv
 
-reduceDM c = case c of
+reduce :: DeMorgan a => Circuit a -> a
+reduce circ = case circ of
   Zero -> zero
   One -> one
   Elem b -> b
-  Par a b -> reduceDM a <||> reduceDM b
-  Seq a b -> reduceDM a <--> reduceDM b
+  Inv a -> inv (reduce a)
+  Par a b -> reduce a <||> reduce b
+  Seq a b -> reduce a <--> reduce b
 
 data Element = R Double
              | C Double
              | L Double
              deriving Show
 
-resistanceAlg el =  case el of
-  R r -> Value r
-  C _ -> Break
-  L _ -> Short
+reduceMap :: DeMorgan c => (a -> c) -> Circuit a -> c
+reduceMap f = reduce . fmap f
 
-connectedAlg el = case el of
-  R _ -> True
-  C _ -> False
-  L _ -> True
-  
-capacityAlg el = case el of
-  R _ -> Short
-  C c -> Value c
-  L _ -> Short
+connected :: Circuit Element -> Bool
+connected = reduceMap f
+  where
+    f el = case el of
+      R _ -> True
+      C _ -> False
+      L _ -> True
 
-impedanceAlg el = Value <$> case el of
-  R r -> \w -> r :+ 0
-  C c -> \w -> 0 :+ (-1)/(w*c)
-  L l -> \w -> 0 :+ (w*l)  
+resistance :: Circuit Element -> Lumped Double
+resistance = fmap getFrac . reduceMap f
+  where
+    f el = Frac <$> case el of
+      R r -> Value r
+      C _ -> Break
+      L _ -> Short
 
-reduceDMWith w f x = reduceDM $ (fmap w . f) <$> x
+impedance :: Circuit Element -> Double -> Lumped (Complex Double)
+impedance = fmap (fmap getFrac) . reduceMap f
+  where
+    f el = (fmap Frac . Value) <$> case el of
+      R r -> \w -> r :+ 0
+      C c -> \w -> 1 / (0 :+ w*c)
+      L l -> \w -> 0 :+ w*l
 
-connected   = reduceDM . fmap connectedAlg
-resistance  = fmap getFrac . reduceDMWith Frac resistanceAlg
-impedance   = fmap (fmap getFrac) . reduceDMWith (fmap Frac) impedanceAlg
-capacity    = fmap (getFrac . getDual) . reduceDMWith (Dual . Frac) capacityAlg
+capacity :: Circuit Element -> Lumped Double
+capacity = fmap (getFrac . getDual) . reduceMap f
+  where
+    f el = Dual . Frac <$> case el of
+      R _ -> Short
+      C c -> Value c
+      L _ -> Short
 
-r = Elem . R
-c = Elem . C
-l = Elem . L
-k True = Zero
-k False = One
+res = Elem . R
+cap = Elem . C
+coil = Elem . L
+key True = One
+key False = Zero
 
-s k1 = r 10 <--> ((r 2 <--> l 5e-3 <--> k k1) <||> c 10e-9)
+s k = res 10 <--> ((res 2 <--> coil 5e-3 <--> key k) <||> cap 10e-9)
 
 ------------------------------------------------------------
 
 data Layer = Insulation { coefficient :: Double
                         , thickness :: Double
                         , area :: Double }
-           | Surface { coefficient :: Double
-                     , area :: Double } deriving Show
+           | Surface    { coefficient :: Double
+                        , area :: Double } deriving Show
 
 thermalResistance :: Circuit Layer -> Double
-thermalResistance = getFrac . reduceDM . fmap (Frac . f)
-  where f (Insulation h l s) = l/(s*h)
-        f (Surface a s) = 1/(s*a)
+thermalResistance = getFrac . reduce . fmap f
+  where
+    f el = Frac $ case el of
+      Insulation h l s -> l/(s*h)
+      Surface a s -> 1/(s*a)
 
-insulation c = fmap Elem . Insulation c
+insulation :: Double -> Double -> Double -> Circuit Layer
+insulation h = fmap Elem . Insulation h
+
+surface :: Double -> Double -> Circuit Layer
 surface = fmap Elem . Surface
 
+concrete, glass, air, pp :: Double -> Double -> Circuit Layer
 concrete = insulation 0.5
 glass = insulation 1.05
 air = insulation 0.024
 pp = insulation 0.07
 
+system :: Circuit Layer
 system = surface 1.5 3 <--> (wall 2 <||> window 1) <--> surface 6 3
   where
     wall = concrete 0.5 <--> pp 0.1
     window = glass 0.005 <--> air 0.01 <--> glass 0.005
 
-flux s dT = dT / thermalResistance s
-
 ------------------------------------------------------------
 
-distribute (Seq a (Par b c)) = distribute $ (a <--> b) <||> (a <--> c)
-distribute (Seq (Par a b) c) = distribute $ (a <--> c) <||> (b <--> c)
-distribute (Seq (Seq a b) c) = distribute $ a <--> (c <--> b)
-distribute (Seq a b) = distribute a <--> distribute b
-distribute (Par a b) = distribute a <||> distribute b
-distribute x = x
+-- distribute :: Circuit t -> Circuit t
+-- distribute (Seq a (Par b c)) = distribute $ (a <--> b) <||> (a <--> c)
+-- distribute (Seq (Par a b) c) = distribute $ (a <--> c) <||> (b <--> c)
+-- distribute (Seq (Seq a b) c) = distribute $ a <--> (c <--> b)
+-- distribute (Seq a b) = distribute a <--> distribute b
+-- distribute (Par a b) = distribute a <||> distribute b
+-- distribute x = x
 
-edges :: Circuit a -> [(a,a)]
-edges = foldMap links . reduceDM . fmap (\x -> [[x]]) . distribute
-  where links lst = zip lst (tail lst)
+-- edges :: Circuit t -> [[t]]
+-- edges = reduce . fmap (\x -> [[x]]) . distribute
+--   where links lst = zip lst (tail lst)
 
-a --> b = a <--> b
-a <+> b = a <||> b
-n = Elem
+-- (-->), (<+>) :: DeMorgan a => a -> a -> a
+-- a --> b = a <--> b
+-- a <+> b = a <||> b
+-- n = Elem
 
-g :: Circuit Int
-g = (n 1 --> (n 3 --> n 4)) <+> (n 2 --> (n 1 <+> n 3)) 
+
+-- g = (n 1 --> (n 3 --> n 4)) <+> (n 2 --> (n 1 <+> n 3)) 
+
+-- -- main = pure ()
 
