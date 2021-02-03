6f457a2c375f7597363736b779c0e0d378a94d13
Sergey Samoylenko HP <samsergey@yandex.ru>
2021-02-03 18:13:32 +1200

    Thinking about workers

--- snippets/Lab2.hs
+++ snippets/Lab2.hs
@@ -2,18 +2,50 @@ module Lab2 where
 
 import Lab1 (mean)
 import Data.List
+import Data.Ord
 
-  
 data Circuit = R Double
                | Par Circuit Circuit
                | Seq Circuit Circuit deriving Show
 
-resistance :: Circuit -> Double
-resistance c = case c of
-  R r -> r
-  Seq c1 c2 -> 1/(1/resistance c1 + 1/resistance c2)
-  Par c1 c2 -> resistance c1 + resistance c2
 
+resistance :: Circuit -> Resistance Double
+resistance c = case c of
+  R r -> Value r
+  Seq c1 c2 -> resistance c1 <&&> resistance c2
+  Par c1 c2 -> resistance c1 <||> resistance c2
+
+------------------------------------------------------------
+
+resistanceToBool :: Resistance a -> Bool
+resistanceToBool Short = True
+resistanceToBool _ = False
+
+boolToResistance :: Bool -> Resistance a
+boolToResistance b = if b then Short else Break
+
+--resistanceToNum :: Num a => Resistance a -> a
+resistanceToNum Short = 0
+resistanceToNum Break = 1/0
+resistanceToNum (Value x) = x
+
+------------------------------------------------------------
+
+data Resistance a = Short | Value a | Break
+  deriving (Show, Eq, Ord)
+
+Break <&&> _ = Break
+Short <&&> r = r
+Value a <&&> Value b = Value $ a + b
+a <&&> b = b <&&> a
+
+inv Short = Break
+inv Break = Short
+inv (Value x) = Value (1/x)
+
+a <||> b = inv (inv a <&&> inv b)
+
+------------------------------------------------------------
 
 bisection :: Eq a
           => (Double -> a)
@@ -51,3 +83,34 @@ bisection' p =
 
 floyd = (\i -> [arsum i + 1 .. arsum (i+1)]) <$> [1..]
   where arsum n = n*(n-1) `div` 2
+
+------------------------------------------------------------
+
+data Worker = A | B | C | D deriving (Show, Eq)
+
+worker w stage = case stage of
+  1 -> case w of
+         A -> R 3
+         B -> R 2
+         C -> R 4
+         D -> R 3
+  2 -> case w of
+         A -> R 6
+         B -> R 4
+         C -> R 5
+         D -> R 4
+         
+pairs = [ (w1, w2)
+        | w1 <- [A, B, C, D]
+        , w2 <- [A, B, C, D]
+        , w1 /= w2]
+
+opt = [ (p1, p2)
+      | p1 <- pairs
+      , p2 <- pairs ]
+
+-- strategy s ((a,b),(c,d)) = case s of
+--    1 -> (worker a 1 <||> worker b 1) <&&>
+--         (worker c 2 <||> worker d 2)
+--    2 -> (worker a 1 <&&> worker b 1) <||>
+--         (worker c 2 <&&> worker d 2)
