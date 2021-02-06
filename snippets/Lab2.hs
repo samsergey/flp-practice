d3f3b0f65f741b94dda3f8488a7f622436f0518a
Sergey at ssb <samsergey@yandex.ru>
2021-02-07 11:31:05 +1200

    Typos and mess

--- snippets/Lab2.hs
+++ snippets/Lab2.hs
@@ -3,7 +3,6 @@ module Lab2 where
 
 import Lab1 (mean)
 import Data.List
-import Data.Ord
 
 data Electronics = R Double
                  | Key Bool
@@ -13,18 +12,19 @@ data Circuit a = Elem a
                | Par (Circuit a) (Circuit a)
                | Seq (Circuit a) (Circuit a) deriving Show
 
-runCircuit elem = go
+runCircuit :: DeMorgan a => (t -> a) -> Circuit t -> a
+runCircuit el = go
   where
     go c = case c of
-      Elem x -> elem x 
+      Elem x -> el x 
       Seq c1 c2 -> go c1 <&&> go c2
       Par c1 c2 -> go c1 <||> go c2
 
 
 resistance :: Circuit Electronics -> Resistance Double
-resistance = runCircuit elem
-  where elem (R r) = Value r
-        elem (Key k) = if k then Short else Break
+resistance = runCircuit el
+  where el (R r) = Value r
+        el (Key k) = if k then Short else Break
 
 ------------------------------------------------------------
 
@@ -35,7 +35,7 @@ resistanceToBool _ = False
 boolToResistance :: Bool -> Resistance a
 boolToResistance b = if b then Short else Break
 
---resistanceToNum :: Num a => Resistance a -> a
+resistanceToNum :: Fractional a => Resistance a -> a
 resistanceToNum Short = 0
 resistanceToNum Break = 1/0
 resistanceToNum (Value x) = x
@@ -91,15 +91,17 @@ bisection test a b
   -- шаг бисекции
   | otherwise = case bisection test a c of
                   Nothing -> bisection test c b
-                  Just c -> Just c
+                  Just c' -> Just c'
   where c = (a + b) / 2
                        
 data Tree a = Node a (Tree a) (Tree a)
              deriving Show
 
+tree :: (t -> (t,t)) -> t -> Tree t
 tree f x = let (a, b) = f x
            in Node x (tree f a) (tree f b)
 
+path :: (a -> Bool) -> Tree a -> [a]
 path p (Node a t1 t2) =
   if p a then [] else [a] ++ path p t1 ++ path p t2 
 
@@ -112,15 +114,15 @@ bisection' p =
   path (\(a, b) -> p a == p b) .
   tree (\(a, b) -> let c = mean a b in ((a,c),(c,b)))
 
-
+floyd :: [[Int]]
 floyd = (\i -> [arsum i + 1 .. arsum (i+1)]) <$> [1..]
   where arsum n = n*(n-1) `div` 2
 
 ------------------------------------------------------------
 
-a 1 = Value 56
-a 2 = Value 17
-b 1 = Value 48
-b 2 = Value 12
-c 1 = Value 52
-c 2 = Value 18
+-- a 1 = Value 56
+-- a 2 = Value 17
+-- b 1 = Value 48
+-- b 2 = Value 12
+-- c 1 = Value 52
+-- c 2 = Value 18
--- snippets/Lab3.hs
+++ snippets/Lab3.hs
@@ -1,39 +1,49 @@
-{-# LANGUAGE TupleSections, BangPatterns #-}
+{-# LANGUAGE TupleSections #-}
 module Main where
 
-import Data.List
-import Data.Monoid
-import GHC.Conc
-import Data.List
-import Data.Maybe
+import Data.List ( find, unfoldr )
+import Data.Maybe ( fromMaybe )
+import Data.Monoid()
+import GHC.Conc ( par, pseq )
+
+toBase' :: Int -> Int -> [Int]
+toBase' b n
+  | b <= 1 = error ""
+  | n == 0 = [0]
+  | otherwise = go [] n
+  where
+    go res 0 = res
+    go res n = go (mod n b : res) (div n b)
 
 toBase :: Int -> Int -> [Int]
--- toBase b 0 = [0]
--- toBase b n = go [] n
---   where
---     go res 0 = res
---     go res n = go (mod n b : res) (div n b)
-
-toBase b n = reverse
-           $ map (`mod` b)
-           $ takeWhile (> 0)
-           $ iterate (`div` b) n
+toBase b n
+  | b <= 1 = error ""
+  | n == 0 = [0]
+  | otherwise = reverse
+                $ map (`mod` b)
+                $ takeWhile (> 0)
+                $ iterate (`div` b) n
 
 pascalStep r = zipWith (+) (0:r) (r++[0])
 
+pascals :: [[Integer]]
 pascals = iterate pascalStep [1]
 
+binomial :: Int -> Int -> Integer
 binomial n k = pascals !! n !! k
 
+bernoulli :: Int -> Int -> Integer
 bernoulli n k = (tail . scanl (+) 0 <$> pascals) !! n !! k
 
+binomial' :: Integral a => a -> a -> a
 binomial' n k = product [n - k + 1..n] `div` product [1..k]
 
-floyd = go 1 [1..]
-  where go i xs = let (a, b) = splitAt i xs
-                  in a : go (i+1) b
+floyd :: [[Int]]
+floyd = let arsum n = (n*(n - 1)) `div` 2
+  in (\i -> [arsum i + 1 .. arsum (i + 1)]) <$> [1..]
 
 
+floyd' :: [[Integer]]
 floyd' = unfoldr (\(i,xs) -> Just $ (i+1,) <$> splitAt i xs) (1,[1..])
 
 floyd'' :: Int -> [[Int]]
@@ -49,19 +59,19 @@ euler h f (x0, y0) = (x0 + h, y0 + h * f x0 y0)
 euler' :: ODESolve
 euler' h f (x0, y0) = case y' of
                       Nothing -> euler h f (x0, y0)
-                      Just y' -> (x, y')
+                      Just r -> (x, r)
   where x = x0 + h
-        y' = findRoot (\y -> h*(f x y) - (y-y0)) y0
+        y' = findRoot (\y -> h * f x y - (y-y0)) y0
 
 rk2' :: ODESolve
 rk2' h f (x0,y0) = case y' of
                       Nothing -> euler h f (x0, y0)
-                      Just y' -> (x, y')
+                      Just y'' -> (x, y'')
   where x = x0 + h
         y' = findRoot (\y -> y0 + h/2*(f x0 y0 + f x y)-y) y0
 
+solveODE :: (t1 -> t2 -> a -> a) -> t2 -> t1 -> a -> [a]
 solveODE method f h = iterate (method h f) 
-
 
 
 -- adaptive :: ODESolve -> ODESolve
@@ -71,48 +81,58 @@ solveODE method f h = iterate (method h f)
 --                             $ ((\h -> method h f (x,y))
 --                             <$> (iterate (/ 2) h))
 
+findRoot' :: (Double -> Double) -> (Double -> Double) -> Double -> Maybe Double
 findRoot' f df = fixedPoint
                  . take 150
                  . iterate (\x -> x - f x / df x)
 
+findRoot :: (Double -> Double) -> Double -> Maybe Double
 findRoot f = findRoot' f (diff f)
 
 fixedPoint :: [Double] -> Maybe Double
 fixedPoint xs = snd <$>
-                (find (\(x1,x2) -> abs(x2-x1) <= 1e-12)
-                 $ zip xs (tail xs))
+                find (\(x1,x2) -> abs(x2-x1) <= 1e-12) 
+                (zip xs (tail xs))
 
+diff :: (Double -> Double) -> Double -> Double
 diff f x = fromMaybe (df 1e-8)
            $ fixedPoint
            $ take 20
            $ df <$> iterate (/ 2) 1e-3
   where df dx = (f (x + dx) - f(x - dx))/(2*dx)
 
+sumsq :: (Num p, Eq p) => p -> p
 sumsq 0 = 0
 sumsq n = n^2 + sumsq (n-1)
 
+sumsq' :: (Num t, Ord t) => t -> t
 sumsq' n = iter 0 1
   where iter s i = if i > n
                    then s
                    else iter (s + i^2) (i + 1)
 
+fib :: (Eq a, Num a, Num p) => a -> p
 fib 1 = 0
 fib 2 = 1
 fib n = fib (n-1) + fib (n-2)
 
 
-fib' n = go 0 1 n
+fib' :: Integer -> Integer
+fib' = go 0 1
   where go a b 1 = a
         go a b 2 = b
         go a b i = go b (a+b) (i-1) 
 
+pfib :: Int -> Integer
 pfib 1 = 0
 pfib 2 = 1
 pfib n = x `par` y `pseq` (x + y)
   where x = pfib (n-1)
         y = pfib (n-2)
 
+main :: IO ()
 main = print $ pfib 40 
 
+floyd''' :: [[Integer]]
 floyd''' = map (\i -> [arsum i + 1 .. arsum (i + 1)]) [1..]
   where arsum n = (n*(n - 1)) `div` 2
