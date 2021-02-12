942333c528f54fd053fc752acc6eb8a9cc98b695
Sergey at ssb <samsergey@yandex.ru>
2021-02-13 09:10:05 +1200

    Gaming example ???

--- snippets/Lab5.hs
+++ snippets/Lab5.hs
@@ -19,4 +19,45 @@ collatz n | even n = n `div` 2
 path' :: Foldable t => (a -> Bool) -> t a -> [a]
 path' p = foldMap $ pure `when` p
 
-facts = 1 : map (*) (tail facts)
+
+
+data M a = I | M [[a]] deriving Show
+
+instance Num a => Semigroup (M a) where
+    I <> x = x
+    x <> I = x
+    M a <> M b = M $ [[dot x y | y <- transpose b ] | x <- a ]
+
+instance Num a => Monoid (M a) where
+    mempty = I
+                 
+dot a b = sum $ zipWith (*) a b                 
+
+jumps = [ (3, 22)
+        , (5, 8)
+        , (11, 26)
+        , (17, 4)
+        , (19, 7)
+        , (20, 29)
+        , (21, 9)
+        , (27, 1)
+        , (30, 30) ]
+
+isJump i j = case lookup i jumps of
+              Nothing -> False
+              Just j' -> j == j'
+
+moves i j  | isJump i j = 1
+           | j == 30 && 30 - i < 6 = 1 - (30 - i)/6
+           | i < j && j <= i+6 = 1/6
+           | otherwise = 0
+
+game = [[moves j i | i <- [1..30] ] | j <- [1..30]]
+
+--times :: (Num n, Integral i) => i -> n -> n
+times 0 _ = mempty
+times 1 a = a
+times 2 a = a <> a
+times n a
+  | even n = (n `div` 2) `times` (a <> a)
+  | odd n  = (n - 1) `times` a <> a
